675aaf059724c07e3c74a58a1af53b2edc286a5f
sanchit-scaler <sanchit.wadehra@scaler.com>
2025-09-16 11:53:57 +0530

    feat: Update root endpoint to render HTML template
    - Modified the root endpoint to return an HTML response using Jinja2 templates.
    - Added necessary imports for Request and HTMLResponse from FastAPI.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+async def read_root(request: Request):
+    return templates.TemplateResponse("todolist.html", {"request": request,"data": "Sample Data from API"})
