cfc2ffaf47e089c9d54aa76091737c5d7e1d7305
sanchit-scaler <sanchit.wadehra@scaler.com>
2025-09-16 12:23:54 +0530

    feat: Load data from JSON file and update todolist template rendering
    - Modified the root endpoint to read data from a JSON file and pass it to the template.
    - Updated the HTML template to iterate over the data and display key-value pairs.

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import json
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def read_root(request: Request):
-    return templates.TemplateResponse("todolist.html", {"request": request,"data": "Sample Data from API"})
+    with open("db.json", "r") as f:
+        db = json.load(f)
+    return templates.TemplateResponse("todolist.html", {"request": request,"data": db})
